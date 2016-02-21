2cb58143ab9650b9c6098467c49542fbf74ede05
Jon Hsu-Kei <hsukei.jon@gmail.com>
2016-02-21 10:17:37 -0600

    [issue-6] Fixed session_state class structure to work how we want it. Added a test method in the hello world in device_handler.

--- device_handler.js
+++ device_handler.js
@@ -1,112 +1,111 @@
-function deviceHandler() {
-    var session_state = new sessionState();
+/**
+ * Created by jhsukei on 2/20/16.
+ */
 
-    /**
-     * Created by jhsukei on 2/20/16.
-     */
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    SessionState = require('./session_state');
 
-    var express = require('express'),
-        bodyParser = require('body-parser'),
-        fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+var app = express();
 
-    var app = express();
-    var MongoClient = require('mongodb').MongoClient;
-    var ObjectId = require('mongodb').ObjectID;
-    var assert = require('assert');
-    var url = 'mongodb://localhost:27017/test';
+app.use(bodyParser.json());
 
-    app.use(bodyParser.json());
+var AUTH_USERS_TABLE = 'authorized_users_table';
+var INVENTORY_TABLE = 'inventory_table';
+var PATIENT_TABLE = 'patient_table';
+var READER_TABLE = 'reader_table';
+var SESSION_TABLE = 'session_table';
 
-    var AUTH_USERS_TABLE = 'authorized_users_table';
-    var INVENTORY_TABLE = 'inventory_table';
-    var PATIENT_TABLE = 'patient_table';
-    var READER_TABLE = 'reader_table';
-    var SESSION_TABLE = 'session_table';
+var session_state = new SessionState(null, [], false, "SESSION_IDLE", [], null);
 
-    //Work around to keep node from crashing. Will require a restart...maybe...idk...
-    //TODO: Implement that DOMAIN/Cluster pattern detailed here.
-    //http://stackoverflow.com/questions/5999373/how-do-i-prevent-node-js-from-crashing-try-catch-doesnt-work
-    process.on('uncaughtException', function (error) {
-        console.log('Server.js Error...continue on.');
-        console.log(error.stack);
-    });
+//Work around to keep node from crashing. Will require a restart...maybe...idk...
+//TODO: Implement that DOMAIN/Cluster pattern detailed here.
+//http://stackoverflow.com/questions/5999373/how-do-i-prevent-node-js-from-crashing-try-catch-doesnt-work
+process.on('uncaughtException', function (error) {
+    console.log('Server.js Error...continue on.');
+    console.log(error.stack);
+});
 
 
-    //Sends the Event Viewer Html
-    app.get('/', function (req, res) {
-        res.send('HELLO WORLD');
-    });
+//Sends the Event Viewer Html
+app.get('/', function (req, res) {
+    res.send('HELLO WORLD');
+    console.log(session_state.state);
+});
 
-    app.post('/rfid', function (req, res) {
-        var currentRFID = req.body.rfid;
-        var deviceID = req.body.deviceid;
-        console.log(currentRFID);
+app.post('/rfid', function (req, res) {
+    var currentRFID = req.body.rfid;
+    var deviceID = req.body.deviceid;
+    console.log(currentRFID);
 
-        MongoClient.connect(url, function (err, db) {
-            assert.equal(null, err);
-            findDocumentByTableAndRFID(db, AUTH_USERS_TABLE, currentRFID, function () {
-                db.close();
-            });
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        findDocumentByTableAndRFID(db, AUTH_USERS_TABLE, currentRFID, function () {
+            db.close();
         });
-        res.sendStatus(200);
     });
+    res.sendStatus(200);
+});
 
-    //Helper Method to get AuthUserByRFID
-    var findDocumentByTableAndRFID = function (db, table, rfid, callback) {
-        var cursor = db.collection(table).find({"authorized_user.rfid": rfid});
-        cursor.each(function (err, doc) {
-            assert.equal(err, null);
-            if (doc == null) {
-                ""
-            }
-            if (doc != null) {
-                console.dir(doc);
-                proccessAuthUsersReturnValues(doc);
-            } else {
-                callback();
-            }
-        });
-    };
-
-    var proccessAuthUsersReturnValues = function (document) {
-        var authUserUuid = document.authorized_user.user_uuid;
-        console.log('\nPLEASE GOD\n' + document.authorized_user.user_uuid);
-        //Match User UUID to open Session
-        MongoClient.connect(url, function (err, db) {
-            assert.equal(null, err);
-            findDocumentByTableAndUserUuid(db, SESSION_TABLE, authUserUuid, function () {
-                db.close();
-            });
-        });
-    };
-
-    var findDocumentByTableAndUserUuid = function (db, table, user_uuid, callback) {
-        var cursor = db.collection(table).find({"session.authorized_users.user_uuid": user_uuid});
-        cursor.each(function (err, doc) {
-            assert.equal(err, null);
-            if (doc != null) {
-                var jsonDoc = JSON.stringify(doc);
-                console.dir(jsonDoc);
-                console.log('\n PLEASE GOD WORK PLEASE\n' + doc.session.taken[0].item_uuid);
-                //Do stuff here
-            } else {
-                console.log('wat');
-                callback();
-            }
-        });
-    };
-
-    //Kills the Node
-    app.post('/exit', function (req, res) {
-        process.exit();
-        res.sendStatus(200);
+//Helper Method to get AuthUserByRFID
+var findDocumentByTableAndRFID = function (db, table, rfid, callback) {
+    var cursor = db.collection(table).find({"authorized_user.rfid": rfid});
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc == null) {
+            ""
+        }
+        if (doc != null) {
+            console.dir(doc);
+            proccessAuthUsersReturnValues(doc);
+        } else {
+            callback();
+        }
     });
+};
 
-    //Instantiate the Server
-    app.use(express.static(__dirname));
-    var server = app.listen(8080, function () {
-        var host = server.address().address;
-        var port = server.address().port;
-        console.log('Listening at http://%s:%s', host, port);
+var proccessAuthUsersReturnValues = function (document) {
+    var authUserUuid = document.authorized_user.user_uuid;
+    console.log('\nPLEASE GOD\n' + document.authorized_user.user_uuid);
+    //Match User UUID to open Session
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        findDocumentByTableAndUserUuid(db, SESSION_TABLE, authUserUuid, function () {
+            db.close();
+        });
     });
-}
+};
+
+var findDocumentByTableAndUserUuid = function (db, table, user_uuid, callback) {
+    var cursor = db.collection(table).find({"session.authorized_users.user_uuid": user_uuid});
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            var jsonDoc = JSON.stringify(doc);
+            console.dir(jsonDoc);
+            console.log('\n PLEASE GOD WORK PLEASE\n' + doc.session.taken[0].item_uuid);
+            //Do stuff here
+        } else {
+            console.log('wat');
+            callback();
+        }
+    });
+};
+
+//Kills the Node
+app.post('/exit', function (req, res) {
+    process.exit();
+    res.sendStatus(200);
+});
+
+//Instantiate the Server
+app.use(express.static(__dirname));
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Listening at http://%s:%s', host, port);
+});
--- session_state.js
+++ session_state.js
@@ -1,116 +1,128 @@
-function sessionState(){
+function SessionState(session, patients, authorized_user_detected, state, authorized_users, last_authorized_user) {
 
-    var session;
-    var patients;
-    var authorized_user_detected = false;
-    var state = "IDLE";
-    var authorized_users;
-    var last_authorized_user;
-
-    function authorizedUserSignedIn(user_uuid){
-        if(!session){
-            console.log("error no session");
-            return;}
-        authorized_user_detected = true;
-        authorized_users.push(user_uuid);
-        last_authorized_user = user_uuid;
-
-        session.actions_log.push(
-            {
-                "timestamp": timestamp(),
-                "action" : "SIGN_IN",
-                "authorized_user" : user_uuid
-            });
-    }
-
-    function patientSignedIn(user_uuid){
-        if(!session){
-            console.log("error no session");
-            return;}
-        patients.push(user_uuid);
-
-        session.actions_log.push(
-            {
-                "timestamp": timestamp(),
-                "action" : "SIGN_IN",
-                "patient" : user_uuid
-            });
-    }
-
-    function sessionDetected(session){
-        this.session = session;
-    }
-
-    function takeItem(item){
-        if(!session){
-            console.log("error no session");
-            return;}
-        if (state == "IDLE"){
-            return;
-        }
-
-        session.taken.push(item);
-        session.actions_log.push(
-            {
-                "timestamp": timestamp(),
-                "action" : "TAKE",
-                "authorized_user" : last_authorized_user
-            });
-    }
-    function authorizedUserSignedOut(user_uuid){
-        if(!session){
-            console.log("error no session");
-            return;}
-        authorized_user_detected = true;
-        authorized_users.push(user_uuid);
-        last_authorized_user = user_uuid;
-
-        var authorized_user_index = authorizedUser.indexOf(user_uuid);
-        authorizedUser.splice(authorized_user_index, 1);
-
-        session.actions_log.push(
-            {
-                "timestamp": timestamp(),
-                "action" : "SIGN_OUT",
-                "authorized_user" : user_uuid
-            });
-    }
-
-    function patientSignedOut(user_uuid){
-        if(!session){
-            console.log("error no session");
-            return;
-        }
-
-        var patient_index = patients.indexOf(user_uuid);
-        patients.splice(patient_index, 1);
-
-        session.actions_log.push(
-            {
-                "timestamp": timestamp(),
-                "action" : "SIGN_OUT",
-                "patient" : user_uuid
-            });
-    }
-    if (authorized_user_detected && session){
-        state = "SESSION_TAKE";
-    }
-
-    var everyone_signed_in = true;
-
-    for each (user in session) {
-        if (!$.in(user.user_uuid, authorized_users) {
-            everyone_signed_in = false;
-        }
-    }
-
-    for each (patient in session) {
-        if (!$.in(patient.user_uuid, authorized_users) {
-            everyone_signed_in = false;
-        }
-    }
-
-    if (everyone_signed_in){
-        state = "SESSION_LIVE";
-    }
+    this.session = session;
+    this.patient = patients;
+    this.authorized_user_detected = authorized_user_detected;
+    this.state = state;
+    this.authorized_users = authorized_users;
+    this.last_authorized_user = last_authorized_user;
 }
+
+SessionState.prototype.authorizedUserSignedIn = function(user_uuid){
+    if(!session){
+        console.log("error no session");
+        return;}
+    authorized_user_detected = true;
+    authorized_users.push(user_uuid);
+    last_authorized_user = user_uuid;
+
+    //Also push to session_table.session.action_log
+    session.actions_log.push(
+        {
+            "timestamp": Date.now(),
+            "action" : "SIGN_IN",
+            "authorized_user" : user_uuid
+        });
+};
+
+SessionState.prototype.patientSignedIn = function(user_uuid){
+    if(!session){
+        console.log("error no session");
+        return;}
+    patients.push(user_uuid);
+
+    //Also push to session_table.session.action_log
+    session.actions_log.push(
+        {
+            "timestamp": Date.now(),
+            "action" : "SIGN_IN",
+            "patient" : user_uuid
+        });
+};
+
+SessionState.prototype.sessionDetected = function(session){
+    this.session = session;
+};
+
+SessionState.prototype.takeItem = function(item){
+    if(!session){
+        console.log("error no session");
+        return;}
+    if (state == "IDLE"){
+        return;
+    }
+
+    //Also push to session_table.session.action_log
+    session.taken.push(item);
+    session.actions_log.push(
+        {
+            "timestamp": Date.now(),
+            "action" : "TAKE",
+            "authorized_user" : last_authorized_user
+        });
+};
+
+SessionState.prototype.authorizedUserSignedOut = function(user_uuid){
+    if(!session){
+        console.log("error no session");
+        return;}
+    authorized_user_detected = true;
+    authorized_users.push(user_uuid);
+    last_authorized_user = user_uuid;
+
+    var authorized_user_index = authorizedUser.indexOf(user_uuid);
+    authorizedUser.splice(authorized_user_index, 1);
+
+    //Also push to session_table.session.action_log
+    session.actions_log.push(
+        {
+            "timestamp": Date.now(),
+            "action" : "SIGN_OUT",
+            "authorized_user" : user_uuid
+        });
+};
+
+SessionState.prototype.patientSignedOut = function(user_uuid){
+    if(!session){
+        console.log("error no session");
+        return;
+    }
+
+    var patient_index = patients.indexOf(user_uuid);
+    patients.splice(patient_index, 1);
+
+    //Also push to session_table.session.action_log
+    session.actions_log.push(
+        {
+            "timestamp": Date.now(),
+            "action" : "SIGN_OUT",
+            "patient" : user_uuid
+        });
+};
+
+SessionState.prototype.updateState = function(){
+
+    return this.state;
+    //if (authorized_user_detected && session){
+    //    this.state = SESSION_TAKE;
+    //}
+    //
+    //var everyone_signed_in = true;
+
+    //for each (user in session) {
+    //    if (!$.in(user.user_uuid, authorized_users) {
+    //        everyone_signed_in = false;
+    //    }
+    //}
+    //
+    //for each (patient in session) {
+    //    if (!$.in(patient.user_uuid, authorized_users) {
+    //        everyone_signed_in = false;
+    //    }
+    //}
+    //if (everyone_signed_in){
+    //    state = "SESSION_LIVE";
+    //}
+};
+
+module.exports = SessionState;
